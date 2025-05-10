c1d61e4cd502e027fff974dfae6445567eeea603
Khurram Ali <khurramcali@gmail.com>
2025-05-10 18:50:00 -0400

    init

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pnpm update --latest \
+  next \
+  react \
+  react-dom \
+  @eslint/eslintrc \
+  @next/eslint-plugin-next \
+  @tailwindcss/postcss \
+  @types/node \
+  @types/react \
+  @types/react-dom \
+  eslint \
+  eslint-config-next \
+  eslint-plugin-react-hooks \
+  tailwindcss \
+  typescript
+
